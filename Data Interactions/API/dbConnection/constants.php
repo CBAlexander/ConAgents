e7ca69574069aac07864222cc59067f464e43cdc
Matthew Frankland <developer@matthewfrankland.co.uk>
2020-02-14 23:02:39 +0000

    added database connection

---
+++ Data Interactions/API/dbConnection/constants.php
@@ -0,0 +1,14 @@
+
+<?php
+
+/**
+ * [define DB Constants]
+ * @var [string]
+ */
+
+define('DB_HOST', 'db5000298692.hosting-data.io');
+define('DB_USERNAME', 'dbu319113');
+define('DB_PASSWORD', 'mykgeh-gIzzez-5ginka');
+define('DB_NAME', 'dbs291857');
+
+?>
